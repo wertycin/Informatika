87bf7ee5beb4a8483afa2d1db94825bf128feb31
wertycin <84451964+wertycin@users.noreply.github.com>
2021-09-11 16:46:08 +0300

    Add files via upload

---
+++ ДЗ 2/ДЗ 2.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+int main()
+{
+    std::vector <std::string> v;
+    std::string s, word, symbol;
+    std::getline(std::cin, s);
+    for (auto i = 0U; i < s.size(); ++i) {
+        if (s[i] == ' ') {
+            v.push_back(word);
+            word = "";
+        }
+        else if (s[i] == ',' || s[i] == '.' || s[i] == '?') {
+            v.push_back(word);
+            word = "";
+            symbol = s[i];
+            v.push_back(symbol);
+        }
+        else {
+            word += s[i];
+        }
+    }
+    v.push_back(word);
+    for (auto i = 0U; i < v.size(); ++i) {
+        std::cout << v[i] << std::endl;
+    }
+    system("pause");
+    return 0;
+}
