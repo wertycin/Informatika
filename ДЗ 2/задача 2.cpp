c57a6ad1874faf29dc86fc592c25f98b25d7a32b
wertycin <84451964+wertycin@users.noreply.github.com>
2021-09-12 21:08:45 +0300

    Add files via upload

---
+++ ДЗ 2/задача 2.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+
+int main()
+{
+	int n, k;
+	std::vector <int> v;
+	std::cout << "enter the number of numbers: ";
+	std::cin >> n;
+	for (auto i = 0U; i < n; ++i)
+	{
+		std::cin >> k;
+		v.push_back(k);
+	}
+	for (auto i = 0U; i < n; ++i)
+	{
+		for (auto j = i; j > 0U; --j)
+		{
+			if (v[j - 1] > v[j])
+			{
+				std::swap(v[j - 1], v[j]);
+			}
+		}
+	}
+	
+	std::cout << "sorted array:" << '\n';
+
+	for (auto i = 0U; i < n; ++i)
+	{
+		std::cout << v[i] << '\n';
+	}
+}
