05f2d0e605e005619b361367a6a851a58081838b
wertycin <84451964+wertycin@users.noreply.github.com>
2021-11-28 17:51:00 +0300

    Add files via upload

---
+++ HW 9 task 1.cpp
@@ -0,0 +1,31 @@
+﻿#include<iostream>
+#include<vector>
+#include<any>
+
+
+template <typename...Types>
+	int Size1(Types...args)
+	{
+		return (sizeof(args) + ...);
+	}
+
+
+
+int Size2()
+{
+	return 0;
+}
+template <typename Type, typename ... Types>
+	int Size2(Type arg, Types ... args)
+	{
+		return sizeof(arg) + Size2(args...);
+	}
+
+
+int main()
+{
+
+	std::cout << Size1('a', 1, 1.f) << std::endl;
+	std::cout << Size2('a', 1, 1.f);
+	return 0;
+}
