130e51799016d3d03c458c004ec22c36f1414a88
wertycin <84451964+wertycin@users.noreply.github.com>
2021-11-29 21:51:50 +0300

    Update task 2.cpp

--- HW 11/task 2.cpp
+++ HW 11/task 2.cpp
@@ -1,5 +1,4 @@
 #include<iostream>
-#include<type_traits>
 template<typename T>
 struct add_const { using type = T const; };
 
@@ -9,22 +8,26 @@ struct remove_const { using type = T; };
 template<typename T>
 struct remove_const<T const> { using type = T; };
 
+template <typename T>
+using add_const_t = typename add_const<T>::type;
 
+template <typename T>
+using remove_const_t = typename remove_const<T>::type;
 
 int main()
 {
-	std::cout << std::boolalpha;
-	int x = 1;
-	int* const y = &x;
-	std::cout << std::is_const_v<decltype(y)> << std::endl;
-	if (std::is_const_v<decltype(y)>)
-	{
-		std::cout << std::is_const_v<remove_const<decltype(y)>::type> << std::endl;
-	}
-	else
-	{
-		std::cout << std::is_const_v<add_const<decltype(y)>::type> << std::endl;
-	}
+    std::cout << std::boolalpha;
+    int x = 1;
+    int* const y = &x;
+    std::cout << std::is_const_v<decltype(y)> << std::endl;
+    if (std::is_const_v<decltype(y)>)
+    {
+        std::cout << std::is_const_v<remove_const_t<decltype(y)>> << std::endl;
+    }
+    else
+    {
+        std::cout << std::is_const_v<add_const_t<decltype(y)>> << std::endl;
+    }
 
-	return 0;
+    return 0;
 }
