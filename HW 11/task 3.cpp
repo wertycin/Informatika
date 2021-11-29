2b43fc28c6100af6335b5986febdc68c44f42cb7
wertycin <84451964+wertycin@users.noreply.github.com>
2021-11-29 21:48:20 +0300

    Update task 3.cpp

--- HW 11/task 3.cpp
+++ HW 11/task 3.cpp
@@ -1,8 +1,7 @@
 #include<iostream>
 
-template<class T>
-struct decay
-{
+template< typename T >
+struct decay {
 private:
     typedef typename std::remove_reference<T>::type U;
 public:
@@ -17,6 +16,9 @@ public:
     >::type type;
 };
 
+template <typename T>
+using decay_t = typename decay<T>::type;
+
 const int f() { return 0; }
 
 int main()
@@ -25,8 +27,8 @@ int main()
 
     const int x = 1;
     std::cout << typeid(x).name() << " is const: " << std::is_const_v<decltype(x)> << std::endl;
-    std::cout << typeid(decay<decltype(x)>::type).name() << " is const: " << std::is_const_v<decay<decltype(x)>::type> << std::endl;
+    std::cout << typeid(decay_t<decltype(x)>).name() << " is const: " << std::is_const_v<decay<decltype(x)>::type> << std::endl;
 
     std::cout << typeid(f).name() << std::endl;
-    std::cout << typeid(decay<decltype(f)>::type).name() << std::endl;
+    std::cout << typeid(decay_t<decltype(f)>).name() << std::endl;
 }
