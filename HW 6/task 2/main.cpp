4bffb043747185151d047abaf15be9746a71d5dd
wertycin <84451964+wertycin@users.noreply.github.com>
2021-11-29 20:05:25 +0300

    Update main.cpp

--- HW 6/task 2/main.cpp
+++ HW 6/task 2/main.cpp
@@ -3,15 +3,25 @@
 using namespace Fraction;
 int main()
 {
-
-	fraction x;
-	fraction y;
-	std::cin >> x;
-	std::cin >> y;
-	std::cout << x << '+' << y << '=' << x + y << std::endl;
-	std::cout << x << '-' << y << '=' << x - y << std::endl;
-	std::cout << x << '*' << y << '=' << x * y << std::endl;
-	std::cout << x << '/' << y << '=' << x / y << std::endl;
-	std::cout << "x after x += y: " << (x += y);
+	try
+	{
+		fraction x;
+		fraction y;
+		std::cin >> x;
+		std::cin >> y;
+		std::cout << x << '+' << y << '=' << x + y << std::endl;
+		std::cout << x << '-' << y << '=' << x - y << std::endl;
+		std::cout << x << '*' << y << '=' << x * y << std::endl;
+		std::cout << x << '/' << y << '=' << x / y << std::endl;
+		std::cout << "x after x += y: " << (x += y);
+	}
+	catch (const std::domain_error& exception)
+	{
+		std::cerr << exception.what() << std::endl;
+	}
+	catch (const std::invalid_argument& exception)
+	{
+		std::cerr << exception.what() << std::endl;
+	}
 	return 0;
 }
