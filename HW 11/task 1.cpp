f6aee5606e2866f4868ac1bb81f012c2e0abf845
wertycin <84451964+wertycin@users.noreply.github.com>
2021-11-29 21:46:57 +0300

    Update task 1.cpp

--- HW 11/task 1.cpp
+++ HW 11/task 1.cpp
@@ -1,39 +1,42 @@
 #include<iostream>
 
-template<class>
+template<typename>
 struct is_function : std::false_type { };
 
 // specialization for regular functions
-template<class Ret, class... Args>
+template<typename Ret, typename... Args>
 struct is_function<Ret(Args...)> : std::true_type {};
 
 // specialization for function types that have cv-qualifiers
-template<class Ret, class... Args>
+template<typename Ret, typename... Args>
 struct is_function<Ret(Args...) const> : std::true_type {};
-template<class Ret, class... Args>
+template<typename Ret, typename... Args>
 struct is_function<Ret(Args...) volatile> : std::true_type {};
-template<class Ret, class... Args>
+template<typename Ret, typename... Args>
 struct is_function<Ret(Args...) const volatile> : std::true_type {};
 
 
 // specialization for function types that have ref-qualifiers
-template<class Ret, class... Args>
+template<typename Ret, typename... Args>
 struct is_function<Ret(Args...)&> : std::true_type {};
-template<class Ret, class... Args>
+template<typename Ret, typename... Args>
 struct is_function<Ret(Args...) const&> : std::true_type {};
-template<class Ret, class... Args>
+template<typename Ret, typename... Args>
 struct is_function<Ret(Args...) volatile&> : std::true_type {};
-template<class Ret, class... Args>
+template<typename Ret, typename... Args>
 struct is_function<Ret(Args...) const volatile&> : std::true_type {};
-template<class Ret, class... Args>
+template<typename Ret, typename... Args>
 struct is_function<Ret(Args...)&&> : std::true_type {};
-template<class Ret, class... Args>
+template<typename Ret, typename... Args>
 struct is_function<Ret(Args...) const&&> : std::true_type {};
-template<class Ret, class... Args>
+template<typename Ret, typename... Args>
 struct is_function<Ret(Args...) volatile&&> : std::true_type {};
-template<class Ret, class... Args>
+template<typename Ret, typename... Args>
 struct is_function<Ret(Args...) const volatile&&> : std::true_type {};
 
+
+template <typename... Types>
+bool is_function_v = is_function<Types...>::value;
 
 class A
 {
@@ -48,7 +51,7 @@ int main()
 	std::cout << std::boolalpha;
 
 	A a(1);
-	std::cout << is_function<decltype(a)>::value << std::endl;
-	std::cout << is_function<decltype(f)>::value << std::endl;
+	std::cout << is_function_v<decltype(a)> << std::endl;
+	std::cout << is_function_v<decltype(f)> << std::endl;
 	return 0;
 }
