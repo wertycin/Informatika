db4ac06d12eb8ce988cbdc771338cd5527210e47
wertycin <84451964+wertycin@users.noreply.github.com>
2021-12-01 02:54:15 +0300

    Create task 3.cpp

---
+++ HW 12/task 3.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+
+double e_n(double d)
+{
+	double e = 1;
+	double f = 1;
+	int k = 1;
+	while (1/f > d)
+	{
+		e = e + (1 / f);
+		k = k + 1;
+		f = f * k;
+	}
+	return e;
+}
+
+int main()
+{
+	double epsilon = 0.0000001;
+	const double x = e_n(epsilon);
+	std::cout << x << std::endl;
+	return 0;
+}
