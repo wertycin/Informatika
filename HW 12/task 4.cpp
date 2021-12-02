e7db3eea98a9c3232b8abfebf2884c3617f6dc0e
wertycin <84451964+wertycin@users.noreply.github.com>
2021-12-02 03:44:21 +0300

    Update task 4.cpp

--- HW 12/task 4.cpp
+++ HW 12/task 4.cpp
@@ -4,30 +4,26 @@
 template <int n>
 constexpr int f()
 {
-	std::array <int, n> array{ 0 };
+	std::array <int, n> array{0};
 	int i = 0;
 	int m = 3;
 	array[0] = 2;
-	while (array[n-1] == 0)
+	while (array[n - 1] == 0)
 	{
 		i = 0;
 		bool flag = 1;
-		while (array[i] != 0)
+		for (; array[i] != 0; ++i)
 		{
 			if (m % array[i] == 0)
 			{
 				flag = 0;
 				break;
 			}
-			++i;
 		}
-		if (flag)
-		{
-				array[i] = m;
-		}
-		m = m + 2;
+		if (flag) { array[i] = m; }
+		m+=2;
 	}
-	return array[n-1];
+	return array[n - 1];
 }
 
 
